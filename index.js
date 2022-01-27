5a227d4ad7985fd836e22373cb619b99d495f9dd
Basant Soni <basantech89@gmail.com>
2022-01-27 20:40:01 +0530

    fix(index): fix the clipath
    Fixed clipath variable value to _dirname

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 "use strict";
 
-const path = require("path");
 const bootstrap = require("commitizen/dist/cli/git-cz").bootstrap;
 
 bootstrap({
-  cliPath: path.join(__dirname, "../../node_modules/commitizen"),
+  cliPath: __dirname,
   config: {
     path: "cz-conventional-changelog",
   },
